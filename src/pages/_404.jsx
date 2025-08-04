ebe0edc7c793b6cc5991f204f9d1ae597cc967a9
Littlebirdwarrior <camille.dorland@gmail.com>
2025-08-04 12:37:12 +0200

    style(404): stylise rapidement la page 404

--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -4,9 +4,9 @@
 export function NotFound() {
   // Affiche un message d'erreur 404
   return (
-    <section style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>404</h1>
-      <p>Page non trouvée.</p>
+    <section style={{ textAlign: 'center', marginTop: '8rem' }}>
+      <h1 style={{ fontSize: '8rem', fontFamily: 'Poppins' }}>404</h1>
+      <p>Oups... Page non trouvée.</p>
     </section>
   );
 }
